20ceef2c2c30ccce4bba2d040d46b9be0ca6e092
Corey Ladovsky <coreyskymusic@gmail.com>
2018-03-10 14:39:30 -0500

    added favicon

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ const req = require('request');
 
 app.use(express.static(`${__dirname}`));
 
-// app.get("/", (request, response) => {
-//   response.sendFile(path.resolve(`${__dirname}/index.html`));
-// });
 
 app.get('/*', (request, response) => {
   req(request.params['0'], (err, res, body) => {
-    // console.log(res);
+    console.log(res.body);
+
+
     response.send(res.body);
   });
+
 });
 
 
-app.listen(port, () => console.log(`working`));
+app.listen(port, () => console.log(`working!`));
